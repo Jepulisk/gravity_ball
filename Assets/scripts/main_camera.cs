ae1970d5e8b0003347ff9a53ad47853c0146999c
Jesper Kallunki <jesper.kallunki@outlook.com>
2019-11-18 17:08:41 +0200

    Commit.

---
+++ Assets/scripts/main_camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_camera : MonoBehaviour {
+    
+    private Camera camera;
+
+    void Awake() {
+        camera = this.GetComponent<Camera>();
+
+        float target_aspect = 9.0f / 16.0f;
+        float window_aspect = (float)Screen.width / (float)Screen.height;
+        float scale_heigth = window_aspect / target_aspect;
+
+        if (scale_heigth < 1.0f) {  
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scale_heigth;
+            rect.x = 0;
+            rect.y = (1.0f - scale_heigth) / 2.0f;
+        
+            camera.rect = rect;
+        } else {
+            float scale_width = 1.0f / scale_heigth;
+
+            Rect rect = camera.rect;
+
+            rect.width = scale_width;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scale_width) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private Rigidbody2D rigidbody;
+    private float rigidbody_dial;
+    public float speed;
+    private Vector3 gravity;
+    public Camera main_camera;
+
+    void Awake() {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        gravity = new Vector3 (Input.acceleration.x, Input.acceleration.y, 0.0f);
+    }
+
+    void FixedUpdate() {
+        rigidbody.velocity = gravity * speed * Time.deltaTime;
+    }
+
+    void LateUpdate() {
+        float y = main_camera.orthographicSize;    
+        float x = y * Screen.width / Screen.height;
+
+        //rigidbody.position = new Vector3 (Mathf.Clamp(rigidbody.position.x, -x, x), Mathf.Clamp(rigidbody.position.y, -y, y), 0.0f);
+
+        if (rigidbody.position.y < -y + 0.5f) {
+            rigidbody.position = new Vector3 (rigidbody.position.x, -y + 0.5f, 0.0f);
+        }
+
+        if (rigidbody.position.y > y - 0.5f) {
+            rigidbody.position = new Vector3 (rigidbody.position.x, y - 0.5f, 0.0f);
+        }
+
+        if (rigidbody.position.x < -x + 0.5f) {
+            rigidbody.position = new Vector3 (-x + 0.5f, rigidbody.position.y, 0.0f);
+        }
+
+        if (rigidbody.position.x > x - 0.5f) {
+            rigidbody.position = new Vector3 (x - 0.5f, rigidbody.position.y, 0.0f);
+        }
+    }
+}

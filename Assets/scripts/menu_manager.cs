efd631a044784b05016d6eb65cb68aff67780362
Jesper Kallunki <jesper.kallunki@outlook.com>
2019-11-19 20:57:28 +0200

    Commit.

--- Assets/scripts/menu_manager.cs
+++ Assets/scripts/menu_manager.cs
@@ -17,6 +17,8 @@ public class menu_manager : MonoBehaviour {
 
     public Canvas pause_menu;
 
+    public Button resume;
+
     void Awake() {
         if (instance == null) {
             instance = this;
@@ -30,13 +32,13 @@ public class menu_manager : MonoBehaviour {
         pause_menu.enabled = false;
 
         play.onClick.AddListener(Play);
-
         options.onClick.AddListener(Options);
+        resume.onClick.AddListener(Resume);
     }
 
     void Update() {
          if(Input.GetKeyDown(KeyCode.Escape)) {
-            MainMenu();
+            Pause();
         }
     }
 
@@ -49,6 +51,16 @@ public class menu_manager : MonoBehaviour {
     void Options() {
         options_menu.enabled = true;
         main_menu.enabled = false;
+    }
+
+    void Pause() {
+        Time.timeScale = 0.0f;
+        pause_menu.enabled = true;
+    }
+
+    void Resume() {
+        pause_menu.enabled = false;
+        Time.timeScale = 1.0f;
     }
 
     void MainMenu() {
